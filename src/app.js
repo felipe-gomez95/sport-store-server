327fdb78a307137b3ea18054318d6583a5c03d9e
Felipe Gomez <luis.gomez@mdy.com.co>
2023-01-09 15:18:32 -0500

    path dependency fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const dbConection = require("./conectDB");
 const router = require("./routes/index.routes");
-const path = require("node:path");
+const path = require("path");
 
 dbConection();
 const app = express();
